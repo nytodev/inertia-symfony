614fda15adc8ff7e14fde39b1c38e01fbe46f2b9
Tony BLARD <56875519+nytodev@users.noreply.github.com>
2026-05-15 10:33:01 +0200

    🐛 fix(service): respect X-Forwarded-Prefix in page object url (#12)

--- src/Service/Inertia.php
+++ src/Service/Inertia.php
@@ -117,10 +117,12 @@ final class Inertia implements ResetInterface
 
         $sharedPropKeys = $this->exposeSharedPropKeys ? array_keys($this->sharedProps) : [];
 
+        $qs = $request->getQueryString();
+
         return $this->inertiaResponse->build(
             $component,
             $mergedProps,
-            $request->getRequestUri(),
+            $request->getBaseUrl().$request->getPathInfo().(null !== $qs ? '?'.$qs : ''),
             $this->version,
             $request,
             $clearHistory,
